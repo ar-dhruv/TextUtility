b5eab262518ade4c5abc4a4ecce0356899cf7136
Dhruv <dev.aroradhruv@gmail.com>
2021-11-23 07:55:28 +0530

    More Functionality Added

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,29 +1,60 @@
 import React, { useState } from "react";
 
 export default function TextForm(props) {
-  const [text, setText] = useState("Enter Text here");
+  const [text, setText] = useState("");
   const handleUpClick = () => {
     let newText = text.toUpperCase();
     setText(newText);
   };
+
+  const handleLowClick = () => {
+    let newText = text.toLowerCase();
+    setText(newText);
+  };
+
+  const handleClearClick = () => {
+    let newText = "";
+    setText(newText);
+  };
+
   const handleOnChange = (event) => {
     setText(event.target.value);
   };
   return (
-    <div>
-      <h1>{props.heading}</h1>
-      <div className="mb-3">
-        <textarea
-          className="form-control"
-          value={text}
-          onChange={handleOnChange}
-          id="myBox"
-          rows="8"
-        ></textarea>
+    <>
+      <div className="container">
+        <h1>{props.heading}</h1>
+        <div className="mb-3">
+          <textarea
+            className="form-control"
+            value={text}
+            onChange={handleOnChange}
+            id="myBox"
+            rows="8"
+          ></textarea>
+        </div>
+        <button className="btn btn-primary my-3 mx-1" onClick={handleUpClick}>
+          Convert to UpperCase
+        </button>
+        <button className="btn btn-primary my-3 mx-1" onClick={handleLowClick}>
+          Convert to LowerCase
+        </button>
+        <button
+          className="btn btn-primary my-3 mx-1"
+          onClick={handleClearClick}
+        >
+          Clear Text
+        </button>
       </div>
-      <button className="btn btn-primary my-3" onClick={handleUpClick}>
-        Convert to UpperCase
-      </button>
-    </div>
+      <div className="container my-3">
+        <h2>Your Text Summary</h2>
+        <p>
+          {text.split(" ").length} words & {text.length} characters
+        </p>
+        <p>{0.008 * text.split(" ").length} minutes read</p>
+        <h2>Preview</h2>
+        <p>{text}</p>
+      </div>
+    </>
   );
 }
